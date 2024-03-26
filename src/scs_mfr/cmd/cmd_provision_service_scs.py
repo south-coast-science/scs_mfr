409683e3879176d8dad524411829727b04a336b2
Bruno Beloff <bruno.beloff@southcoastscience.com>
2024-03-26 15:06:40 +0000

    Added AWS project validation to provisioning

--- src/scs_mfr/cmd/cmd_provision_service_scs.py
+++ src/scs_mfr/cmd/cmd_provision_service_scs.py
@@ -94,22 +94,22 @@ class CmdProvisionServiceSCS(object):
 
     @property
     def project(self):
-        return bool(self.__opts.project)
+        return self.__opts.project is not None
 
 
     @property
     def project_org(self):
-        return self.__opts.project[0] if self.__opts.project else None
+        return None if self.__opts.project is None else self.__opts.project[0]
 
 
     @property
     def project_group(self):
-        return self.__opts.project[1] if self.__opts.project else None
+        return None if self.__opts.project is None else self.__opts.project[1]
 
 
     @property
     def project_location(self):
-        return self.__opts.project[2] if self.__opts.project else None
+        return None if self.__opts.project is None else self.__opts.project[2]
 
 
     @property
